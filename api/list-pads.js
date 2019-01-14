13564f1aacb8b337e49e4a83c0e87485d997be73
Chmichev Boris <boris362@yandex.ru>
2019-01-14 18:28:49 +0300

    list pads api method

---
+++ api/list-pads.js
@@ -0,0 +1,17 @@
+const fetchFactory = require('../lib/fetch/fetch-factory')
+const l18n = require('../lib/l18n/l18n-decorator')
+
+const query =
+  `*[_type == "pad"] {
+    title,
+    "id": _id,
+    "parent": parents._ref
+  }`
+
+module.exports = (sanityProject, sanityStorageName) =>
+  fetchFactory(
+    sanityProject,
+    sanityStorageName,
+    query,
+    l18n
+  )

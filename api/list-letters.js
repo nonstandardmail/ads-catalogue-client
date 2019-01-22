4cad72ecd6e877d410e2ea68d44da0df7cfe2c7c
Chmichev Boris <boris362@yandex.ru>
2019-01-22 15:02:52 +0300

    Pass project info to letter renderer

--- api/list-letters.js
+++ api/list-letters.js
@@ -2,8 +2,8 @@ const { map, pipe } = require('ramda')
 const fetchFactory = require('../lib/fetch/fetch-factory')
 const parseRecipients = require('../lib/transforms/letter/parse-recipients')
 const blocksToHtml = require('@sanity/block-content-to-html')
-const bodyToHTML = target => {
-  target.body = blocksToHtml({ blocks: target.body })
+const bodyToHTML = (projectId, dataset) => target => {
+  target.body = blocksToHtml({ blocks: target.body, projectId, dataset })
   return target
 }
 
@@ -20,5 +20,8 @@ module.exports = (sanityProject, sanityStorageName) =>
     sanityProject,
     sanityStorageName,
     query,
-    pipe(map(bodyToHTML), map(parseRecipients))
+    pipe(
+      map(bodyToHTML(sanityProject, sanityStorageName)),
+      map(parseRecipients)
+    )
   )

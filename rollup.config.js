73476a7f7ccc0043e2c7aa76ac8897391513dc3e
Широченков Алексей <a.shirochenkov@corp.mail.ru>
2019-02-19 18:17:08 +0300

    add build umd

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import json from "rollup-plugin-json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+
+import pkg from "./package.json";
+
+export default {
+  input: "index.js",
+  output: {
+    name: "ads-catalogue-client",
+    file: pkg.browser,
+    format: "umd"
+  },
+  plugins: [
+    json(),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      preferBuiltins: false
+    }),
+    commonjs(),
+    babel({
+      runtimeHelpers: true,
+      exclude: "node_modules/**" // only transpile our source code
+    })
+  ]
+};
